d1bb6b96a831e796124bd1ae0c8f63b68d9b6789
ManasviAtGitHub <manasvisharma821@gmail.com>
2020-07-26 19:44:42 +0530

    --different consumer example included

---
+++ Web Services/Temperature Example/DifferentConsumer/DifferentConsumer/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DifferentConsumer.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Result"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="CelsiusToFarenheit" OnClick="Button1_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="FarenheitToCesius" OnClick="Button2_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web Services/Temperature Example/DifferentConsumer/DifferentConsumer/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DifferentConsumer.ServiceReference1;
+namespace DifferentConsumer
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Temperature_InterfaceClient c = new Temperature_InterfaceClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+            String text = TextBox1.Text;
+            Double val = Convert.ToDouble(text);
+            Double result = c.celsius_to_farenheit(val);
+            Label1.Text = Convert.ToString(result);
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String text = TextBox1.Text;
+            Double val = Convert.ToDouble(text);
+            Double result = c.farenheit_to_celsius(val);
+            Label1.Text = Convert.ToString(result);
+        }
+    }
+}
